7cf3918f57a3ba10fa7f8b8fc4ca89af478a3da5
haquochuu <huu3675@gmail.com>
2024-06-29 09:45:38 +0700

    finish #5-#9

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,11 +1,45 @@
 import React from 'react';
-
+//  this is comment
 class MyComponent extends React.Component {
+
+    state = {
+        firstName: '',
+        lastName: ''
+    }
+
+    handleOnChangeFName = event => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleOnChangeLName = event => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleOnClick = () => {
+        alert('click button')
+    }
+
     render() {
+        let name = 'AAA'
         return (
+            <>
+                <form>
+                    <label htmlFor="fname">First name:</label><br />
+                    <input type="text" value={this.state.firstName}
+                        onChange={event => this.handleOnChangeFName(event)} /><br />
+                    <label htmlFor="lname">Last name:</label><br />
+                    <input type="text" value={this.state.lastName}
+                        onChange={event => this.handleOnChangeLName(event)} /><br /><br />
+                    <input type="button" value="Submit"
+                        onClick={() => this.handleOnClick()}
+                    />
+                </form>
 
-            <div> My CPN</div>
-
+            </>
         )
     }
 }
