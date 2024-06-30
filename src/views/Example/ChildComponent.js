127f24aa3527bd7934326b328a9fce0a19ea8620
haquochuu <huu3675@gmail.com>
2024-06-30 21:42:52 +0700

    finish #13 - #14

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -4,43 +4,87 @@ import React from 'react';
 //  this is comment
 class ChildComponent extends React.Component {
 
-    handleOnChangeFName = event => {
-        this.setState({
-            firstName: event.target.value
-        })
+    //     handleOnChangeFName = event => {
+    //         this.setState({
+    //             firstName: event.target.value
+    //         })
+    //     }
+
+    //     handleOnChangeLName = event => {
+    //         this.setState({
+    //             lastName: event.target.value
+    //         })
+    //     }
+
+    //     handleOnClick = () => {
+    //         alert('click button')
+    //     }
+
+    //     render() {
+    //         let { line, name, jobs } = this.props
+    //         return (
+    //             <>
+
+    //                 <div className='job-list'>
+    //                     {
+    //                         jobs && jobs.map((item, index) => {
+    //                             return (
+    //                                 <div key={item.id}>
+
+    //                                     {item.id + " " + item.title + " " + item.salary}
+
+    //                                 </div>
+    //                             )
+    //                         })
+    //                     }
+
+    //                 </div>
+    //             </>
+    //         )
+    //     }
+    // }
+
+    state = {
+        showJobs: true
     }
 
-    handleOnChangeLName = event => {
+    handleShowHide = () => {
         this.setState({
-            lastName: event.target.value
+            showJobs: !this.state.showJobs
         })
-    }
-
-    handleOnClick = () => {
-        alert('click button')
     }
 
     render() {
-        let { line, name, jobs } = this.props
+        let { jobs } = this.props
+        let { showJobs } = this.state
         return (
             <>
+                {showJobs === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>show</button>
+                    </div>
+                    :
+                    <div><div className='job-list'>
+                        {
+                            jobs.map((item, index) => {
+                                return (
+                                    <div key={item.id}>
 
-                <div className='job-list'>
-                    {
-                        jobs && jobs.map((item, index) => {
-                            return (
-                                <div key={item.id}>
+                                        {item.id + " " + item.title + " " + item.salary}
 
-                                    {item.id + " " + item.title + " " + item.salary}
-
-                                </div>
-                            )
-                        })
-                    }
-
-                </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>hide</button>
+                        </div>
+                    </div>
+                }
             </>
         )
+
     }
 }
 
